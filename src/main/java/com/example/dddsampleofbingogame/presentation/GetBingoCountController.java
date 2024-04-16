f4d5c82d198b05ceb66e241b99aef0317a5bd021
yoshihiro_yoneyama <yoshihiro.yoneyama@salesnavi.co.jp>
2024-04-16 22:41:20 +0900

    GetBingoCountControllerパス修正

--- src/main/java/com/example/dddsampleofbingogame/presentation/GetBingoCountController.java
+++ src/main/java/com/example/dddsampleofbingogame/presentation/GetBingoCountController.java
@@ -5,7 +5,9 @@ import com.example.dddsampleofbingogame.application.service.bingo.GetBingoCountS
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GetBingoCountController {
   @NonNull GetBingoCountService getBingoCountService;
 
-  @GetMapping()
-  public GetBingoCountResponse get(@NonNull final GetBingoCountRequest request) {
+  @PostMapping("api")
+  public GetBingoCountResponse get(@RequestBody final GetBingoCountRequest request) {
     final var output = getBingoCountService.getBingoCount(
       new GetBingoCountInput(
         request.maxSideLength(),
